1d49be7fcde172fc6eb67bee57720908b793e6b6
Hubery Hsu <hananiahhsu@live.cn>
2026-02-09 22:33:44 +0800

    【AL-82】OCCT SDK

---
+++ sdk/msvc2022-x64-md/Debug/occt/cmake/OpenCASCADEDataExchangeTargets-debug.cmake
@@ -0,0 +1,139 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "TKDE" for configuration "Debug"
+set_property(TARGET TKDE APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDE PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDE.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDE.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDE )
+list(APPEND _cmake_import_check_files_for_TKDE "${_IMPORT_PREFIX}/win64/vc14/libd/TKDE.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDE.dll" )
+
+# Import target "TKXSBase" for configuration "Debug"
+set_property(TARGET TKXSBase APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKXSBase PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKXSBase.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKXSBase.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKXSBase )
+list(APPEND _cmake_import_check_files_for_TKXSBase "${_IMPORT_PREFIX}/win64/vc14/libd/TKXSBase.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKXSBase.dll" )
+
+# Import target "TKDESTEP" for configuration "Debug"
+set_property(TARGET TKDESTEP APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDESTEP PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDESTEP.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDESTEP.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDESTEP )
+list(APPEND _cmake_import_check_files_for_TKDESTEP "${_IMPORT_PREFIX}/win64/vc14/libd/TKDESTEP.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDESTEP.dll" )
+
+# Import target "TKXCAF" for configuration "Debug"
+set_property(TARGET TKXCAF APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKXCAF PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKXCAF.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKXCAF.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKXCAF )
+list(APPEND _cmake_import_check_files_for_TKXCAF "${_IMPORT_PREFIX}/win64/vc14/libd/TKXCAF.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKXCAF.dll" )
+
+# Import target "TKDEIGES" for configuration "Debug"
+set_property(TARGET TKDEIGES APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDEIGES PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEIGES.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEIGES.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDEIGES )
+list(APPEND _cmake_import_check_files_for_TKDEIGES "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEIGES.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEIGES.dll" )
+
+# Import target "TKDESTL" for configuration "Debug"
+set_property(TARGET TKDESTL APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDESTL PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDESTL.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDESTL.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDESTL )
+list(APPEND _cmake_import_check_files_for_TKDESTL "${_IMPORT_PREFIX}/win64/vc14/libd/TKDESTL.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDESTL.dll" )
+
+# Import target "TKDEVRML" for configuration "Debug"
+set_property(TARGET TKDEVRML APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDEVRML PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEVRML.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEVRML.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDEVRML )
+list(APPEND _cmake_import_check_files_for_TKDEVRML "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEVRML.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEVRML.dll" )
+
+# Import target "TKRWMesh" for configuration "Debug"
+set_property(TARGET TKRWMesh APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKRWMesh PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKRWMesh.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKRWMesh.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKRWMesh )
+list(APPEND _cmake_import_check_files_for_TKRWMesh "${_IMPORT_PREFIX}/win64/vc14/libd/TKRWMesh.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKRWMesh.dll" )
+
+# Import target "TKDECascade" for configuration "Debug"
+set_property(TARGET TKDECascade APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDECascade PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDECascade.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDECascade.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDECascade )
+list(APPEND _cmake_import_check_files_for_TKDECascade "${_IMPORT_PREFIX}/win64/vc14/libd/TKDECascade.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDECascade.dll" )
+
+# Import target "TKBinXCAF" for configuration "Debug"
+set_property(TARGET TKBinXCAF APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKBinXCAF PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKBinXCAF.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKBinXCAF.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKBinXCAF )
+list(APPEND _cmake_import_check_files_for_TKBinXCAF "${_IMPORT_PREFIX}/win64/vc14/libd/TKBinXCAF.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKBinXCAF.dll" )
+
+# Import target "TKXmlXCAF" for configuration "Debug"
+set_property(TARGET TKXmlXCAF APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKXmlXCAF PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKXmlXCAF.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKXmlXCAF.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKXmlXCAF )
+list(APPEND _cmake_import_check_files_for_TKXmlXCAF "${_IMPORT_PREFIX}/win64/vc14/libd/TKXmlXCAF.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKXmlXCAF.dll" )
+
+# Import target "TKDEOBJ" for configuration "Debug"
+set_property(TARGET TKDEOBJ APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDEOBJ PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEOBJ.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEOBJ.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDEOBJ )
+list(APPEND _cmake_import_check_files_for_TKDEOBJ "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEOBJ.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEOBJ.dll" )
+
+# Import target "TKDEPLY" for configuration "Debug"
+set_property(TARGET TKDEPLY APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(TKDEPLY PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEPLY.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEPLY.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDEPLY )
+list(APPEND _cmake_import_check_files_for_TKDEPLY "${_IMPORT_PREFIX}/win64/vc14/libd/TKDEPLY.lib" "${_IMPORT_PREFIX}/win64/vc14/bind/TKDEPLY.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
